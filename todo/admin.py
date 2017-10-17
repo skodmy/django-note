8e7af61dd264e5b3ed52d8d375ab24eb8d1f38f9
Skochynskiy Dmytro <skodmy@gmail.com>
2017-10-17 09:54:03 +0300

    todo app admin models added

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from.models import Todo, TodoList, TodoListItem
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    """
+    Admin class for Todo.
+    """
+    date_hierarchy = 'created_at'
+    ordering = '-last_modified_at'
+    search_fields = ('name', )
+
+
+@admin.register(TodoList)
+class TodoListAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TodoListItem)
+class TodoListItemAdmin(admin.ModelAdmin):
+    list_filter = ('checked', admin.BooleanFieldListFilter)
