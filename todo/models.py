a8c0fca97eb8f5020ee89bad2a2ea36d86f5414f
Skochynskiy Dmytro <skodmy@gmail.com>
2017-10-17 09:25:21 +0300

    models added in todo app

--- todo/models.py
+++ todo/models.py
@@ -1,22 +1,48 @@
+"""
+todo app models goes here.
+"""
 from django.db import models
 from django.contrib.auth.models import User
 
 
-class ToDoList(models.Model):
+class Todo(models.Model):
+    """
+    Base abstract model for todo app models.
+    """
     name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} created at {}, last modified at {}'.format(
+            self.name,
+            self.created_at,
+            self.last_modified_at
+        )
+
+    class Meta:
+        abstract = True
+
+
+class TodoList(Todo):
+    """
+    A class that represents a user's todo list.
+    """
     user = models.ForeignKey(User)
-    creation_date = models.DateTimeField(auto_created=True)
-    last_modification_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
+    def is_completed(self):
+        """
+        Checks if all items for self are checked.
+
+        :return: True if all are True, False otherwise.
+        """
+        return all((todo_list_item.checked for todo_list_item in TodoListItem.objects.filter(todo_list=self)))
 
 
-class ToDoListItem(models.Model):
-    title = models.CharField(max_length=300)
+class TodoListItem(Todo):
+    """
+    A class which objects represent todo list items.
+    """
+    todo_list = models.ForeignKey(TodoList)
     text = models.TextField()
-    creation_date = models.DateTimeField(auto_created=True)
-    last_modification_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
+    checked = models.BooleanField(default=False)
