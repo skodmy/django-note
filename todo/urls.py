ee37ca635450df2a35a3ce0be52a7402cd338502
Skochynskiy Dmytro <skodmy@gmail.com>
2017-10-22 10:15:49 +0300

    todo in dirty base state

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
-from .views import TodoListListView, TodoListDetailView, TodoListItemDetailView, Index
+from .views import TodoListDetailView, TodoListItemDetailView, Index, TodoListCreateView, \
+    TodoListUpdateView, TodoListDeleteView, TodoListItemCreateView, TodoListItemUpdateView, TodoListItemDeleteView
 
 urlpatterns = [
-    url(r'^$', Index.as_view()),
-    url(r'^lists/$', TodoListListView.as_view()),
-    url(r'^list/(?P<pk>\d+)/$', TodoListDetailView.as_view()),
-    url(r'^list-item/(?P<pk>\d+)/$', TodoListItemDetailView.as_view()),
+    url(r'^$', Index.as_view(), name='index'),
+    url(r'^list/add/$', TodoListCreateView.as_view(), name='list-add'),
+    url(r'^list/(?P<pk>\d+)/update/$', TodoListUpdateView.as_view(), name='list-update'),
+    url(r'^list/(?P<pk>\d+)/delete/$', TodoListDeleteView.as_view(), name='list-delete'),
+    url(r'^list/(?P<pk>\d+)/$', TodoListDetailView.as_view(), name='list-detail'),
+    url(r'^list/(?P<id>\d+)/list-item/(?P<pk>\d+)/$', TodoListItemDetailView.as_view(), name='list-item-detail'),
+    url(r'^list/(?P<pk>\d+)/list-item/add/$', TodoListItemCreateView.as_view(), name='list-item-add'),
+    url(r'^list/(?P<id>\d+)/list-item/(?P<pk>\d+)/update/$', TodoListItemUpdateView.as_view(), name='list-item-update'),
+    url(r'^list/(?P<id>\d+)/list-item/(?P<pk>\d+)/delete/$', TodoListItemDeleteView.as_view(), name='list-item-delete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,37 @@
 # from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from .models import TodoList, TodoListItem
 
 
-class TodoListListView(LoginRequiredMixin, ListView):
+class TodoListCreateView(LoginRequiredMixin, CreateView):
+    model = TodoList
+    fields = ['name']
+    template_name = 'todo/list_add.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TodoListUpdateView(LoginRequiredMixin, UpdateView):
+    model = TodoList
+    fields = ['name']
+    template_name = 'todo/list_update.html'
+
+
+class TodoListDeleteView(LoginRequiredMixin, DeleteView):
+    model = TodoList
+    template_name = 'todo/list_delete.html'
+    success_url = reverse_lazy('todo:index')
+
+
+class Index(LoginRequiredMixin, ListView):
     model = TodoList
     context_object_name = 'todo_lists'
-    template_name = 'todo/lists_list.html'
+    template_name = 'todo/index.html'
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
@@ -25,5 +49,33 @@ class TodoListItemDetailView(LoginRequiredMixin, DetailView):
     template_name = 'todo/list_item_detail.html'
 
 
-class Index(TodoListListView):
-    template_name = 'todo/index.html'
+class TodoListItemCreateView(LoginRequiredMixin, CreateView):
+    model = TodoListItem
+    fields = ['title', 'text']
+    context_object_name = 'todo_list_item'
+    template_name = 'todo/list_item_add.html'
+
+    def form_valid(self, form):
+        form.instance.todo_list = TodoList.objects.get(id=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('todo:list-detail', kwargs={'pk': self.kwargs['pk']})
+
+
+class TodoListItemUpdateView(LoginRequiredMixin, UpdateView):
+    model = TodoListItem
+    fields = ['title', 'text', 'done']
+    context_object_name = 'todo_list_item'
+    template_name = 'todo/list_item_update.html'
+
+    def get_success_url(self):
+        return reverse('todo:list-detail', kwargs={'pk': self.kwargs['id']})
+
+
+class TodoListItemDeleteView(LoginRequiredMixin, DeleteView):
+    model = TodoListItem
+    template_name = 'todo/list_item_delete.html'
+
+    def get_success_url(self):
+        return reverse('todo:list-detail', kwargs={'pk': self.kwargs['id']})
