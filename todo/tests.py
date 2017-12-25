ede2441737830b94494e864aff84454641da7fab
Skochynskiy Dmytro <skodmy@gmail.com>
2017-12-25 18:03:20 +0200

    views tests added

--- todo/tests.py
+++ todo/tests.py
@@ -1,5 +1,10 @@
-from django.test import TestCase
-from .models import TodoList, TodoListItem, User
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
+
+from .models import TodoList, TodoListItem
+from .views import Index
+from .views import TodoListCreateView
+from .views import TodoListItemCreateView, TodoListItemUpdateView, TodoListItemDeleteView
 
 
 class TodoListTestCase(TestCase):
@@ -23,3 +28,119 @@ class TodoListItemTestCase(TestCase):
 
     def test_done(self):
         self.assertFalse(self.todo_list_item.done)
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self._view = None
+        self._request_factory = RequestFactory()
+        self._user = AnonymousUser()
+
+
+class ConcreteUserViewTestCase(ViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self._user = User.objects.create(username='tester', email='tester@test.com', password='test_secret')
+
+
+class IndexViewTestCase(ConcreteUserViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self._view = Index()
+        self._view.request = self._request_factory.get('/')
+        self._view.request.user = self._user
+
+    def test_get_queryset(self):
+        for todo_list in self._view.get_queryset():
+            self.assertEqual(todo_list.user, self._user)
+
+    def test_as_view(self):
+        response = Index.as_view()(self._view.request)
+        self.assertEqual(response.status_code, 200)
+
+
+class CRUDViewCommonMixinTestCase(TestCase):
+    pass
+
+
+class TodoListCRUDViewCommonMixinTestCase(TestCase):
+    pass
+
+
+class TodoListCreateViewTestCase(ConcreteUserViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self._view = TodoListCreateView()
+        self._view.request = self._request_factory.get('/list/add/')
+        self._view.request.user = self._user
+
+    def test_form_valid(self):
+        form = self._view.get_form_class()({'name': 'test_list'})
+        self.assertEqual(self._view.form_valid(form).status_code, 302)
+
+    def test_as_view(self):
+        response = TodoListCreateView.as_view()(self._view.request)
+        self.assertEqual(response.status_code, 200)
+
+
+class TodoListDetailViewTestCase(TestCase):
+    pass
+
+
+class TodoListUpdateViewTestCase(TestCase):
+    pass
+
+
+class TodoListDeleteViewTestCase(TestCase):
+    pass
+
+
+class TodoListItemCRUDViewCommonMixinTestCase(TestCase):
+    pass
+
+
+class TodoListItemCreateViewTestCase(ConcreteUserViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self._view = TodoListItemCreateView()
+        self._view.request = self._request_factory.get('/list/item/add/')
+        self._view.request.user = self._user
+        test_todo_list = TodoList.objects.create(name='test todo list', user=self._user)
+        setattr(self._view, 'kwargs', {'todo_list_id': test_todo_list.id})
+
+    def test_form_valid(self):
+        form = self._view.get_form_class()({'title': 'test_list item', 'text': 'test_list item text'})
+        self.assertEqual(self._view.form_valid(form).status_code, 302)
+
+    def test_get_success_url(self):
+        self.assertNotEqual(self._view.get_success_url(), '')
+
+    def test_as_view(self):
+        response = TodoListItemCreateView.as_view()(self._view.request)
+        self.assertEqual(response.status_code, 200)
+
+
+class TodoListItemDetailViewTestCase(ConcreteUserViewTestCase):
+    pass
+
+
+class TodoListItemUpdateViewTestCase(ConcreteUserViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self._view = TodoListItemUpdateView()
+        test_todo_list = TodoList.objects.create(name='test todo list', user=self._user)
+        setattr(self._view, 'kwargs', {'todo_list_id': test_todo_list.id})
+
+    def test_get_success_url(self):
+        self.assertNotEqual(self._view.get_success_url(), '')
+
+
+class TodoListItemDeleteViewTestCase(ConcreteUserViewTestCase):
+    def setUp(self):
+        super().setUp()
+        self._view = TodoListItemDeleteView()
+        test_todo_list = TodoList.objects.create(name='test todo list', user=self._user)
+        setattr(self._view, 'kwargs', {'todo_list_id': test_todo_list.id})
+
+    def test_get_success_url(self):
+        self.assertNotEqual(self._view.get_success_url(), '')
