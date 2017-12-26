2077fc07678d715ded5ce32678c62820d5123663
Skochynskiy Dmytro <skodmy@gmail.com>
2017-12-26 17:44:52 +0200

    updated due to new release

--- django-note/setup.py
+++ django-note/setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-note',
-    version='1.0.0',
+    version='1.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
